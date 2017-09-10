a217c52e0d888ef5a0ca22e9c02f205aa438bcc1
Jesse Shaw <jdshaw1987@gmail.com>
2017-09-10 08:48:13 -0400

    Updated mongodb url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
 global.DATABASE_URL || 
-'mongodb://localhost/blog-app';
+'mongodb://admin:Mbdtf87!@ds129024.mlab.com:29024/blogapi';
 exports.PORT = process.env.PORT || 8080;
