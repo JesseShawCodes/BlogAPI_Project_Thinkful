4d03bdb3926a13cdc1ec702521aafb572a147ef2
Jesse Shaw <jdshaw1987@gmail.com>
2017-09-09 22:52:15 -0400

    Updated Blog for Mongoose compatability

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const {PORT, DATABASE_URL} = require('./config');
+
+mongoose.Promise = global.Promise;
 
 const blogRouter = require('./blogpostrouter');
 const app = express();
 
 app.use(morgan('common'));
 app.use('/blogposts', blogRouter);
+
+
 
 // both runServer and closeServer need to access the same
 // server object, so we declare `server` here, and then when
@@ -15,32 +23,39 @@ let server;
 // this function starts our server and returns a Promise.
 // In our test code, we need a way of asynchronously starting
 // our server, since we'll be dealing with promises there.
-function runServer() {
-  const port = process.env.PORT || 8080;
-  return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+  
+    return new Promise((resolve, reject) => {
+      mongoose.connect(databaseUrl, err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app.listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+      });
     });
-  });
-}
+  }
 
 // like `runServer`, this function also needs to return a promise.
 // `server.close` does not return a promise on its own, so we manually
 // create one.
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
-      }
-      resolve();
-    });
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
   });
 }
 
